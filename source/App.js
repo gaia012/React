10e37da17bcee162a04f958533785e5e7525d75c
Taeo(이종렬) <gaia012@coupang.com>
2017-05-16 16:19:14 +0900

    KanbanBoard

--- source/App.js
+++ source/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
+import KanbanBoard from './KanbanBoard';
 
-class GroceryList extends React.Component {
-	render(){
-		return (
-			<ul>
-				<ListItem quantity="1" name="Bread" />
-				<ListItem quantity="2" name="Eggs" />
-				<ListItem quantity="3" name="Milk" />
-			</ul>
-		)
-	}
-}
+let cardsList = [
+	{
+		id: 1,
+		title: "Read the Book",
+		description: "I should read the whole book",
+		status: "in-progress",
+		tasks: []
+	},
+	{
+		id: 2,
+		title: "Write some code",
+		description: "Code along with the samples in the book",
+		status: "todo",
+		tasks: [
+			{
+				id: 1,
+				name: "ContactList Example",
+				done: true
+			},
+			{
+				id: 2,
+				name: "Kanban Example",
+				done: false
+			},
+			{
+				id: 3,
+				name: "My own experiments",
+				done: false
+			}
+		]
+	},
+];
 
-class ListItem extends React.Component{
-	render(){
-		return (
-			<li>
-				{this.props.quantity} * {this.props.name}
-			</li>
-		)
-	}
-}
-React.render(<GroceryList />, document.getElementById('root'));
+
+React.render(<KanbanBoard cards={cardsList}/>, document.getElementById('root'));
