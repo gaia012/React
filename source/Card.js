85b78531b493daed20f61b650cf18573d5ca8188
Taeo(이종렬) <gaia012@coupang.com>
2017-05-28 15:53:26 +0900

    KanbanBoard V2

--- source/Card.js
+++ source/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CheckList from './CheckList';
+import marked from 'marked';
 
 class Card extends React.Component {
 	constructor() {
@@ -9,12 +10,16 @@ class Card extends React.Component {
 		};
 	}
 
+	toggleDetails(){
+		this.status({showDetails: !this.state.showDetails});
+	}
+
 	render() {
 		let cardDetails;
 		if (this.state.showDetails) {
 			cardDetails = (
 				<div className="card__details">
-					{this.props.description}
+					<span dangerouslySetInnerHTML= {{__html:marked(this.props.description)}} />
 					<CheckList cardId={this.props.id} tasks={this.props.tasks}/>
 				</div>
 			);
@@ -22,9 +27,9 @@ class Card extends React.Component {
 
 		return (
 			<div className="card">
-				<div className="card__title" onClick={
-					() => this.setState({showDetails: !this.state.showDetails})
-				}>{this.props.title}</div>
+				<div className={this.state.showDetails?"card__title card__title--is-open":"card__title"} onClick={this.toggleDetails().bind(this)}>
+					{this.props.title}
+				</div>
 				{cardDetails}
 			</div>
 
